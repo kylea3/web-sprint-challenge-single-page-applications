f34237cbc527f2b2fb81d4b06470ca9edf2281bd
Kyle Andrus <kyleandrus3@gmail.com>
2022-12-09 16:53:19 -0700

    create homepage layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import './App.css';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>    
+      <header>
+        <h1>Lambda Eats</h1>
+        <button id="home">Home</button>
+      </header>
+      <div className="heroImage">
+        <h2>Your favorite food, delivered while coding</h2>
+        <button className="pizzaFormBtn">Pizza?</button>
+      </div>
+    </div>
   );
 };
 export default App;
